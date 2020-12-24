633be98567140a758be8e416e1208631fcc385de
lanxuexing <lanxuexing313wsr@163.com>
2020-12-24 23:10:14 +0800

    feat: default avatar in ColumnList component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 </template>
 
 <script lang="ts">
+/**
+ * bootstrap 5.0 文档
+ * @link https://getbootstrap.com/docs/5.0/getting-started/introduction/
+ */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { defineComponent } from 'vue'
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
@@ -18,7 +22,19 @@ const testData: ColumnProps[] = [
   {
     id: 2,
     title: 'test2的专栏',
-    description: '这里是test2专栏，有一段非常有意思的简介，可以更新一下噢',
+    description: '这里是test2专栏，有一段非常有意思的简介，可以更新一下噢'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这里是test3专栏，有一段非常有意思的简介，可以更新一下噢',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这里是test4专栏，有一段非常有意思的简介，可以更新一下噢',
     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
   }
 ]
