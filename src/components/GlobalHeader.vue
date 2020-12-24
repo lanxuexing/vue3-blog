4f5f2753378a5baab8bad7eda3598289ad7d3b13
lanxuexing <lanxuexing313wsr@163.com>
2020-12-24 23:55:47 +0800

    feat: add Dropdown and DropdownItem component

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -12,7 +12,17 @@
     </ul>
     <ul v-if="user.isLogin" class="list-inline mb-0">
       <li class="list-inline-item">
-        <a href="#" class="btn btn-outline-light my-2">你好，{{ user.name }}</a>
+        <dropdown :title="`你好，${user.name}`">
+          <dropdown-item>
+            <a href="#" class="dropdown-item">新建博客</a>
+          </dropdown-item>
+          <dropdown-item disabled>
+            <a href="#" class="dropdown-item">编辑资料</a>
+          </dropdown-item>
+          <dropdown-item>
+            <a href="#" class="dropdown-item">退出登录</a>
+          </dropdown-item>
+        </dropdown>
       </li>
     </ul>
   </nav>
@@ -20,6 +30,8 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import Dropdown from '@/components/Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 
 export interface UserProps {
   isLogin: boolean;
@@ -34,6 +46,10 @@ export default defineComponent({
       type: Object as PropType<UserProps>,
       required: true
     }
+  },
+  components: {
+    Dropdown,
+    DropdownItem
   }
 })
 </script>
