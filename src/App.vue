30962164cfc9d2b14fd3cf9a008fd98871c200d1
lanxuexing <lanxuexing313wsr@163.com>
2020-12-27 01:13:02 +0800

    feat: migration Home

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div class="container">
     <global-header :user="currentUser"></global-header>
     <router-view></router-view>
-    <!-- <column-list :list="list"></column-list> -->
     <footer class="text-center py-4 text-secondary bg-light mt-6">
       <small>
         <ul class="list-inline mb-0">
@@ -24,34 +23,8 @@
  */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { defineComponent } from 'vue'
-// import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
-// const testData: ColumnProps[] = [
-//   {
-//     id: 1,
-//     title: 'test1的专栏',
-//     description: '这里是test1专栏，有一段非常有意思的简介，可以更新一下噢',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
-//   },
-//   {
-//     id: 2,
-//     title: 'test2的专栏',
-//     description: '这里是test2专栏，有一段非常有意思的简介，可以更新一下噢'
-//     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
-//   },
-//   {
-//     id: 3,
-//     title: 'test3的专栏',
-//     description: '这里是test3专栏，有一段非常有意思的简介，可以更新一下噢',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
-//   },
-//   {
-//     id: 4,
-//     title: 'test4的专栏',
-//     description: '这里是test4专栏，有一段非常有意思的简介，可以更新一下噢',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
-//   }
-// ]
+
 const currentUser: UserProps = {
   isLogin: true,
   name: 'muziyu',
@@ -61,12 +34,10 @@ const currentUser: UserProps = {
 export default defineComponent({
   name: 'App',
   components: {
-    // ColumnList,
     GlobalHeader
   },
   setup () {
     return {
-      // list: testData,
       currentUser
     }
   }
---
+++ src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home-page">
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <img src="@/asstes/callout.svg" alt="callout" class="w-50" />
+          <h2 class="font-weight-light">随心写作，自由表达</h2>
+          <p>
+            <a href="#" class="btn btn-primary my-2">开始写博客</a>
+          </p>
+        </div>
+      </div>
+    </section>
+    <h4 class="font-weight-bold text-center">发现精彩</h4>
+    <column-list :list="list"></column-list>
+    <button class="btn btn-outline-primary mt-2 mb-5 mx-auto btn-block w-25">
+      加载更多
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这里是test1专栏，有一段非常有意思的简介，可以更新一下噢',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这里是test2专栏，有一段非常有意思的简介，可以更新一下噢'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这里是test3专栏，有一段非常有意思的简介，可以更新一下噢',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这里是test4专栏，有一段非常有意思的简介，可以更新一下噢',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg'
+  }
+]
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
