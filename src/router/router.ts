68bbf26faa8e5a008a2a2f2bfff3385e44e9e0ab
lanxuexing <lanxuexing313wsr@163.com>
2020-12-27 23:06:17 +0800

    feat: global before guards

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,6 +3,7 @@ import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import ColumnDetail from '@/components/column/ColumnDetail.vue'
 import CreatePost from '@/views/CreatePost.vue'
+import store from '@/vuex/store/store'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -28,6 +29,16 @@ const router = createRouter({
       component: CreatePost
     }
   ]
+})
+
+// 全局前置守卫：https://next.router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
+router.beforeEach((to, from, next) => {
+  console.log(to, from, next)
+  if (to.name !== 'login' && !store.state.user.isLogin) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
