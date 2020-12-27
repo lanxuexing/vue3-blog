292a06863aedf2927c0b10a6644ccfdb38bb207a
lanxuexing <lanxuexing313wsr@163.com>
2020-12-28 00:03:57 +0800

    feat: axios Access-Control-Allow-Origin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from '@/router/router'
 import store from '@/vuex/store/store'
 
+// 测试前段跨域代码
+import axios from 'axios'
+axios.get('/api/columns?currentPage=1&pageSize=5').then(res => {
+  console.log(res.data)
+})
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
