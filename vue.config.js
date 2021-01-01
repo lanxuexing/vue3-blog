e01a315fab645a218d6d37c57b756e093b9280c0
lanxuexing <lanxuexing313wsr@163.com>
2021-01-01 22:27:49 +0800

    feat: docs

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,12 @@ module.exports = {
   outputDir: 'docs',
   devServer: {
     open: true,
-    host: '0.0.0.0',
+    host: 'localhost',
     port: 8080,
     https: false,
     // 以上的ip和端口是我们本机的;下面为需要跨域的，配置文档API：https://cli.vuejs.org/zh/config/#devserver-proxy
     proxy: { // 配置跨域
-      'http://api.vikingship.xyz/api': {
+      '/api': {
         target: 'http://api.vikingship.xyz/api/', // 这里后台的地址模拟的;应该填写你们真实的后台接口
         ws: true,
         changOrigin: true, // 允许跨域
